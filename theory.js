bc03d16f24ba205baee07b4507e22e64c3e30367
Vamsee1317 <vamsee.achieversit@gmail.com>
2025-07-25 12:06:14 +0530

    Git Commands File Added

---
+++ theory.js
@@ -0,0 +1,31 @@
+// Git Commands :
+
+// git --v - To check the version of git
+// git status - To check the status of the repository
+// git init - To initialize a new repository
+// git add . - To add all the files in the repository
+// git commit -m "message" - To commit the changes with a message
+// git log - To check the commit history
+// git branch - To check the branches
+// git branch <branch-name> - To create a new branch
+// git checkout <branch-name> - To switch to a branch
+// git merge <branch-name> - To merge a branch
+// git remote add origin <repository-url> - To add a remote repository
+// git push - To push the changes to the remote repository
+// git pull - To pull the changes from the remote repository
+// git clone <repository-url> - To clone a repository
+// git remote rm <repository-url> - To remove a remote repository
+
+
+// Frequent Git Commands :
+
+// git status
+// git add .
+// git commit -m "message"
+// git log
+                // git branch
+                // git checkout <branch-name>
+                // git merge <branch-name>
+// git push
+// git pull
+// git clone <repository-url>
